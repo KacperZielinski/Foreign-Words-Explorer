01dd9eb198e279ff075ab39bac8e12abacd443fa
Kacper Zieliński <kacper.zielinski@mailfence.com>
2019-06-16 12:29:40 +0200

    Default phone locale changed to US

--- app/src/main/java/com/zielinski/kacper/fwe/MainActivity.kt
+++ app/src/main/java/com/zielinski/kacper/fwe/MainActivity.kt
@@ -56,7 +56,7 @@ class MainActivity : AppCompatActivity() {
     private fun getWordFromSpeech() {
         val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.UK)
 
         if (intent.resolveActivity(packageManager) != null) {
             startActivityForResult(intent, SPEECH_RESULT_ACTIVITY_CODE)
